8363bb7e4a09e3815afa11d0ec2723e4354983c0
psh94 <suran5281@gmail.com>
2025-03-04 09:48:37 +0900

    기초 - 스택(10828번)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+
     }
 }
